2d9d8c15e133cd1ce906801fc8a040e47db44131
krowki96 <265697030+krowki96@users.noreply.github.com>
2026-08-24 23:52:27 -0600

    Add NVIDIA akmod; replace fedora-multimedia with RPM Fusion

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,8 +12,38 @@ cp -avf "/ctx/system_files"/. /
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# Disable multimedia repo
+for repo in /etc/yum.repos.d/*multimedia*.repo; do \
+    [ -f "$repo" ] && sed -i 's/^enabled=1/enabled=0/' "$repo"; \
+done
+
+# Install RPM Fusion repo
+dnf5 install -y \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+
+# Install NVIDIA 580.xx driver akmod for legacy Pascal cards, and build requirements
+dnf5 install -y \
+    akmod-nvidia-580xx \
+    xorg-x11-drv-nvidia-580xx \
+    xorg-x11-drv-nvidia-580xx-cuda \
+    kernel-devel \
+    kernel-headers \
+    gcc \
+    make \
+    elfutils-libelf-devel \
+    kmodtool \
+    mokutil
+
+# Compile driver for this build's kernel
+kver="$(rpm -q --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-core | tail -n1)"; \
+    akmods --force --kernels "$kver"; \
+    depmod -a "$kver"
+
 # this installs a package from fedora repos
 dnf5 install -y tmux steam
+
+dnf5 clean all
 
 # Use a COPR Example:
 #
